f1543407d15e1e8c8bc2c35f1dbeabad0f4819ad
Johan Sandoval <jojo.han718@gmail.com>
2023-04-30 16:17:39 -0500

    Timesheets automatically update and have proper syntax

--- src/components/AvailableTimes.js
+++ src/components/AvailableTimes.js
@@ -4,7 +4,7 @@ function AvailableTimes(date, trainer) {
   const [items, setItems] = useState([])
 
   useEffect(() => {
-    fetch(date + '-AM-Shift.json').then(response => response.json()).then((json) => {
+    fetch("time-sheets/" + date + '-AM-Shift.json').then(response => response.json()).then((json) => {
       var times = []
       if(trainer === "Any"){
         for (var j = 0; j < json.length; j++) {
@@ -19,16 +19,7 @@ function AvailableTimes(date, trainer) {
 
       else {
         for (var i = 0; i < json.length; i++) {
-          var words = json[i].name.split(' ');
-          for (let i = 0; i < words.length; i++) {
-            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-            words[i] = words[i].replace('*', '');
-            words[i] = words[i].replace('(', '');
-            words[i] = words[i].replace(')', '');
-          }
-          words = words.join(" ");
-          var final = words.split(', ');
-          if(trainer === final[1] + final[0]){
+          if(trainer === json[i].name){
             for( const element of Object.keys(json[i]) ){
               if(json[i][element] === "Available"){
                 times.push(element)
@@ -40,7 +31,7 @@ function AvailableTimes(date, trainer) {
       setItems(times)
     });
 
-    fetch(date + '-PM-Shift.json').then(response => response.json()).then((json) => {
+    fetch("time-sheets/" + date + '-PM-Shift.json').then(response => response.json()).then((json) => {
       if(trainer === "Any"){
         for (var j = 0; j < json.length; j++) {
           for( const element of Object.keys(json[j]) ){
@@ -54,16 +45,7 @@ function AvailableTimes(date, trainer) {
 
       else {
         for (var i = 0; i < json.length; i++) {
-          var words = json[i].name.split(' ');
-          for (let i = 0; i < words.length; i++) {
-            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-            words[i] = words[i].replace('*', '');
-            words[i] = words[i].replace('(', '');
-            words[i] = words[i].replace(')', '');
-          }
-          words = words.join(" ");
-          var final = words.split(', ');
-          if(trainer === final[1] + final[0]){
+          if(trainer === json[i].name){
             for( const element of Object.keys(json[i]) ){
               if(json[i][element] === "Available"){
                 setItems(current => [...current, element])
--- src/components/GetTrainers.js
+++ src/components/GetTrainers.js
@@ -7,16 +7,6 @@ function GetTrainers() {
     fetch('trainers.json').then(response => response.json()).then((json) => {
       var trainers = [];
       for (var i = 0; i < json.length; i++) {
-        // var words = json[i].name.split(' ');
-        // for (let i = 0; i < words.length; i++) {
-        //   words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-        //   words[i] = words[i].replace('*', '');
-        //   words[i] = words[i].replace('(', '');
-        //   words[i] = words[i].replace(')', '');
-        // }
-        // words = words.join(" ");
-        // var final = words.split(', ');
-        // trainers.push([final[1] + final[0], json[i].number]);
         trainers.push([json[i].name, json[i].number]);
       }
       setItems(trainers)
