22e3c1968a37dc27011dbc1ea4726fdb50e2a841
Johan Sandoval <jojo.han718@gmail.com>
2023-05-03 22:36:55 -0500

    Time sheets no longer separated by AM and PM which is reflected in the AvailableTimes function

--- src/components/AvailableTimes.js
+++ src/components/AvailableTimes.js
@@ -5,7 +5,7 @@ function AvailableTimes(date, trainer) {
   const [itemsT, setItemsT] = useState([])
 
   useEffect(() => {
-    fetch("time-sheets/" + date + '-AM-Shift.json').then(response => response.json())
+    fetch("time-sheets/" + date + '.json').then(response => response.json())
       .then((json) => {
         var times = []
         var trainers = []
@@ -20,6 +20,9 @@ function AvailableTimes(date, trainer) {
               }
             }
           }
+          times.sort(function (a, b) {
+            return new Date('1970/01/01 ' + a) - new Date('1970/01/01 ' + b);
+          });
         }
 
         else {
@@ -33,60 +36,8 @@ function AvailableTimes(date, trainer) {
             }
           }
         }
-        times.sort(function (a, b) {
-          if ((a === '12:00 AM') !== (b === '12:00 AM')) {
-            return a === '12:00 AM' ? -1 : 1;
-        }
-          let x = a.replace(/[^0-9]/g, ""),
-              y = b.replace(/[^0-9]/g, "");
-          return x - y
-        });
         setItems(times)
         setItemsT(trainers)
-      })
-      .catch(() => {
-        setItems([])
-        setItemsT([])
-      });
-
-    fetch("time-sheets/" + date + '-PM-Shift.json').then(response => response.json())
-      .then((json) => {
-        var times = []
-        var trainers = []
-        if(trainer === "Any"){
-          for (var j = 0; j < json.length; j++) {
-            for( const element of Object.keys(json[j]) ){
-              if(json[j][element] === "Available"){
-                if(!(times.includes(element))){
-                  times.push(element)
-                }
-                trainers.push([json[j].name, element])
-              }
-            }
-          }
-        }
-
-        else {
-          for (var i = 0; i < json.length; i++) {
-            if(trainer === json[i].name){
-              for( const element of Object.keys(json[i]) ){
-                if(json[i][element] === "Available"){
-                  times.push(element)
-                }
-              }
-            }
-          }
-        }
-        times.sort(function (a, b) {
-          if ((a === '12:00 PM') !== (b === '12:00 PM')) {
-            return a === '12:00 PM' ? -1 : 1;
-        }
-          let x = a.replace(/[^0-9]/g, ""),
-              y = b.replace(/[^0-9]/g, "");
-          return x - y
-        });
-        setItems(current => [...current, ...times])
-        setItemsT(current => [...current, ...trainers])
       })
       .catch(() => {
         setItems([])
